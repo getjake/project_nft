9d8c5e7f13977212aed874af4f32dd1755f65ffa
jake <bozi.chinese@gmail.com>
2021-11-20 10:20:49 +0800

    deployed to Mumbai testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Compile contract
+  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorld");
+  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorld", "SPRM"); // inside params: name and symbol
+  
+  await superMarioWorld.deployed();
+  console.log("Success! Contract was deployed to: ", superMarioWorld.address);
 
-  await greeter.deployed();
+  // Mint NFT
+  await superMarioWorld.mint("https://ipfs.io/ipfs/QmY8VNgpqnXT4kUaZMpGo3AQvwkeHpwymPs6QpvaQ8miXx")
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("NFT successfully minted!");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
